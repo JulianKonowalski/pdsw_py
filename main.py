a9944723b3273da373ccaec38cbdd547d45036c7
JulianKonowalski <julian.konwalski@gmail.com>
2025-10-23 12:37:02 +0200

    remove logging from main

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import logging
 from src.gui.Interface import Interface
 
-LOGGER: logging.Logger = logging.getLogger(__name__)
-logging.basicConfig(filename="interface.log", level=logging.INFO)
-
 if __name__ == "__main__":
-    LOGGER.info("Starting main")
-
     interface: Interface = Interface()
     interface.run()
