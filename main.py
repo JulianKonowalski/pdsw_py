426887118ee990188dd3a1b4b5ca8cae7f454884
JulianKonowalski <julian.konwalski@gmail.com>
2025-10-23 13:29:13 +0200

    b

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from src.gui.Interface import Interface
+# from src.gui.Interface import Interface
+from src.spi.SpiBus import SpiBus
 
 if __name__ == "__main__":
-    interface: Interface = Interface()
-    interface.run()
+    # interface: Interface = Interface()
+    # interface.run()
+
+    spibus = SpiBus()
+
+
+    while True:
+        carData = spibus.getCarData()
+        print(carData.getData())
